86da61c37ce579f181ce5e5255d591d417764716
Vadim Savin <savinvadim1312@gmail.com>
2024-03-08 15:25:06 +0000

    Configure Expo Router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello there from Expo Router</Text>
     </View>
   );
 }
